c3dfabd9b4229a58c688080dac046c55c5da9903
KamolchanokSinraksa <77113968+zolocapricorn@users.noreply.github.com>
2021-06-11 16:52:21 +0700

    Update Color Histogram.py

--- Color Histogram.py
+++ Color Histogram.py
@@ -6,3 +6,4 @@ import time
 #Opens up web driver and goes to Google Images
 driver = webdriver.Firefox()
 driver.get("https://www.google.com/search?q=orange&client=firefox-b-d&sxsrf=ALeKk02rFLZJDczUwawr91XzwZKUChRX3Q:1623329826893&source=lnms&tbm=isch&sa=X&ved=2ahUKEwjclb3tjo3xAhXJzjgGHS26BJgQ_AUoAXoECAEQAw&biw=1536&bih=711")
+
