e8e5ada0d03d582dda9aa18f0b91072a191f081d
Kamolchanok Sinraksa <77113968+zolocapricorn@users.noreply.github.com>
2021-06-30 21:38:25 +0700

    Upgrade code.

--- Dataset-by-selenium.py
+++ Dataset-by-selenium.py
@@ -1,16 +1,18 @@
 #Imports Packages
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
+import os
 import time
 import urllib.request
-import os
+
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from tqdm import tqdm
 
 #Opens up web driver and goes to Google Images
-driver = webdriver.Chrome("C:/Users/Lenovo/Downloads/chromedriver.exe")
+driver = webdriver.Chrome("C:/Program Files/Google/Chrome/Application/chromedriver.exe")
 driver.get("https://www.google.com/")
 
 box = driver.find_element_by_xpath("/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/input")
-box.send_keys("apple fruit")
+box.send_keys("apples fruit")
 box.send_keys(Keys.ENTER)
 
 driver.find_element_by_xpath('//*[@id="hdtb-msb"]/div[1]/div/div[2]/a').click()
@@ -30,22 +32,20 @@ while True:
         break
     last_height = new_height
 
-for p in range(1, 105):
+for p in tqdm(range(1, 105)):
     try:
         img = driver.find_element_by_xpath('//*[@id="islrg"]/div[1]/div['+str(p)+']/a[1]/div[1]/img')
         src = img.get_attribute('src')
-        urllib.request.urlretrieve(src, "E:/Github/Color-Histogram-by-Python/Dataset from Dataset.py/apples/b "+str(p)+".jpg")
+        urllib.request.urlretrieve(src, "E:/Github/Color-Histogram-by-Python/Dataset from selenium/apples/b "+str(p)+".jpg")
     except:
         pass
 
 def main():
-    paths = "E:/Github/Color-Histogram-by-Python/Dataset from Dataset.py/apples/"
-    for count, filename in enumerate(os.listdir(paths)):
+    paths = "E:/Github/Color-Histogram-by-Python/Dataset from selenium/apples/"
+    for count, filename in tqdm(enumerate(os.listdir(paths))):
         dst = "apple " + str(count+1) +".jpg"
         src = paths+filename
         dst = paths+dst
         os.rename(src, dst)
 if __name__ == '__main__':
     main()
-
-#ภาพที่ 25 หาร 25 ลงตัว จะไม่สามารถรันได้
