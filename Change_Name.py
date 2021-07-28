4320c02960ab64502c1aa1d38d23cff15827fea6
Kamolchanok Sinraksa <77113968+zolocapricorn@users.noreply.github.com>
2021-07-29 02:42:59 +0700

    Create Change_Name.py

---
+++ Change_Name.py
@@ -0,0 +1,12 @@
+import os
+def main():
+    lists = [""]
+    for name in lists:
+        paths = "E:/Github/Color-Histogram-by-Python/storage/Apples/sum/"
+        for count, filename in enumerate(os.listdir(paths)):
+            dst = "%02d.jpg" % (count+1)
+            src = paths+filename
+            dst = paths+dst
+            os.rename(src, dst)
+if __name__ == '__main__':
+    main()
